6539681b3629bcec3c6c42ce014ec5ac8669f055
dantesworld <dantesworld2@gmail.com>
2025-02-26 15:15:19 +0200

    used JpaREpo, impl soft delete for Book entity

--- src/main/java/mate/academy/bookapp/service/BookService.java
+++ src/main/java/mate/academy/bookapp/service/BookService.java
@@ -10,4 +10,6 @@ public interface BookService {
     BookDto getById(Long id);
 
     List<BookDto> findAll();
+
+    void deleteById(Long id);
 }
--- src/main/java/mate/academy/bookapp/service/BookServiceImpl.java
+++ src/main/java/mate/academy/bookapp/service/BookServiceImpl.java
@@ -35,4 +35,9 @@ public class BookServiceImpl implements BookService {
                 .map(bookMapper::toDto)
                 .toList();
     }
+
+    @Override
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
 }
